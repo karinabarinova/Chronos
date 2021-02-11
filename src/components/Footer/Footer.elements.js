6481bfb95566cb5448f0784a6ba8a21e6affb6b5
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-02-11 14:02:53 +0200

    Added Footer info columns

--- src/components/Footer/Footer.elements.js
+++ src/components/Footer/Footer.elements.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import { Link } from 'react-router-dom';
 export const FooterContainer = styled.div`
     background-color: #101522;
     padding: 4rem 0 2rem 0;
@@ -56,5 +56,56 @@ export const FormInput = styled.input`
 
     @media screen and (max-width: 820px) {
         margin: 0 0 16px 0;
+    }
+`;
+
+export const FooterLinksContainer = styled.div`
+    width: 100%;
+    max-width: 1000px;
+    display: flex;
+    justify-content: center;
+
+    @media screen and (max-width: 820px) {
+        padding-top: 32px;
+    }
+`;
+
+export const FooterLinksWrapper = styled.div`
+    display: flex;
+
+    @media screen and (max-width: 820px) {
+        flex-direction: column;
+    }
+`;
+
+export const FooterLinksItems = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 16px;
+    text-align: center;
+    width: 160px;
+    box-sizing: border-box;
+    color: #fff;
+
+    @media screen and (max-width: 420px) {
+        margin: 0;
+        padding: 10px;
+        width: 100%;
+    }
+`;
+
+export const FooterLinkTitle = styled.h2`
+    margin-bottom: 16px;
+`;
+
+export const FooterLink = styled(Link)`
+    color: #fff;
+    text-decoration: none;
+    margin-bottom: 0.5rem;
+
+    &:hover {
+        color: #0467fb;
+        transition: 0.3s ease-out;
     }
 `;
