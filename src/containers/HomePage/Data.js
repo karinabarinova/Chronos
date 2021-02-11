128500154dee2ccd965d872f10d0c1164b84d5c4
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-02-11 13:26:23 +0200

    added svg images for other Info blocks

--- src/containers/HomePage/Data.js
+++ src/containers/HomePage/Data.js
@@ -1,7 +1,6 @@
 export const homeObjOne = {
     lightBg: false,
     primary: true,
-    lightBg: false, 
     imgStart: '', 
     lightTopLine: true, 
     lightTextDesc: true,
@@ -12,5 +11,53 @@ export const homeObjOne = {
     topLine: 'Marketing Agency',
     start: '',
     img: require('../../images/svg-1.svg').default,
+    alt: 'Image'
+}
+
+export const homeObjTwo = {
+    primary: false,
+    lightBg: true, 
+    imgStart: 'start', 
+    lightTopLine: false, 
+    lightTextDesc: false,
+    buttonLabel: 'Get Started', 
+    description: 'We help business owners increase their revenue. Out team of unique specialists can help you achieve your business goals.', 
+    headline: 'Lead Generation Specialist for Online Business', 
+    lightText: false, 
+    topLine: 'Marketing Agency',
+    start: 'true',
+    img: require('../../images/svg-2.svg').default,
+    alt: 'Image'
+}
+
+export const homeObjThree = {
+    lightBg: false,
+    primary: true,
+    imgStart: '', 
+    lightTopLine: true, 
+    lightTextDesc: true,
+    buttonLabel: 'Get Started', 
+    description: 'We help business owners increase their revenue. Out team of unique specialists can help you achieve your business goals.', 
+    headline: 'Lead Generation Specialist for Online Business', 
+    lightText: true, 
+    topLine: 'Marketing Agency',
+    start: '',
+    img: require('../../images/svg-3.svg').default,
+    alt: 'Image'
+}
+
+export const homeObjFour = {
+    lightBg: false,
+    primary: true,
+    imgStart: 'start', 
+    lightTopLine: true, 
+    lightTextDesc: true,
+    buttonLabel: 'Get Started', 
+    description: 'We help business owners increase their revenue. Out team of unique specialists can help you achieve your business goals.', 
+    headline: 'Lead Generation Specialist for Online Business', 
+    lightText: true, 
+    topLine: 'Marketing Agency',
+    start: 'true',
+    img: require('../../images/svg-4.svg').default,
     alt: 'Image'
 }
