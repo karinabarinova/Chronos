ea2e0b81c308e0265ce68214517e0f53568779b3
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-02-12 14:47:22 +0200

    add auth state check to the App component, added logout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from './store/index';
 import GlobalStyle from './globalStyles';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, withRouter } from 'react-router-dom';
 import { Navbar, Footer } from './components'
 import Home from './containers/HomePage/Home';
 import Services from './containers/Services/Services';
@@ -12,25 +14,58 @@ import SingIn from './containers/SignIn/SingIn';
 import ResetPassword from './containers/ResetPassword/ResetPassword';
 import ResetPasswordValidate from './containers/ResetPassword/ResetPasswordValidate'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <ScrollToTop />
-      <Navbar />
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
+  render() {
+    let routes = (
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/services" exact component={Services} />
-        <Route path="/products" exact component={Products} />
-        <Route path="/sign-up" exact component={SingUp} />
-        <Route path="/sign-in" exact component={SingIn} />
-        <Route path="/reset-password" exact component={ResetPassword} />
-        <Route path="/reset-password-confirm" exact component={ResetPasswordValidate} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/sign-up" exact component={SingUp} />
+          <Route path="/sign-in" exact component={SingIn} />
+          <Route path="/reset-password" exact component={ResetPassword} />
+          <Route path="/reset-password-confirm" exact component={ResetPasswordValidate} />
       </Switch>
-      <Footer />
-      {/* <Switch></Switch> */}
-    </BrowserRouter>
-  );
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/logout" component={Logout} />
+          {/* <Route path="/account" component={Account} />
+          <Route component={NotFound} */}
+        </Switch>
+      )
+    }
+    return (
+      <BrowserRouter>
+        <GlobalStyle />
+        <ScrollToTop />
+        <Navbar />
+        {routes}
+        <Footer />
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.jwtToken != null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps))(App);
