ec8d5af750782bbe73c1c103a7ff6f787309e659
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-02-17 10:58:28 +0200

    changed EventView field

--- src/containers/Account/EventView.js
+++ src/containers/Account/EventView.js
@@ -3,13 +3,28 @@ import React from 'react'
 const EventView = (props) => {
     return (
         <div>
-            <h3>Create an event</h3>
-            <label> Name
-            <input type="text" />
-            </label>
-            <label> Description
-            <input type="text" />
-            </label>
+            <div>
+                <div><h3>Create an event</h3></div>
+                <button>x</button>
+            </div>
+            <form>
+                <label>What do you want to do?</label>
+                <div>
+                    <input type="text" placeholder="e.g.: Buy Milk"></input>
+                    <div>
+                        <button>colors</button>
+                    </div>
+                </div>
+                <label>When?</label>
+                <div>
+                    <input type="date" />
+                    <input type="time" />
+                </div>
+                <label>Who will participate?</label>
+                <div>
+                    <input type="text" />
+                </div>
+            </form>
         </div>
     )
 }
